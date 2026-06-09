363a7e69d2a02d21a9ba8080d9355025868927d2
Enjambre Group <dfv.0624@hotmail.com>
2026-06-09 18:24:20 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const poppins = Poppins({
@@ -23,6 +24,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es" className={poppins.variable}>
+      <head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-332CP37NS9"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-332CP37NS9');
+          `}
+        </Script>
+      </head>
       <body>{children}</body>
     </html>
   );
